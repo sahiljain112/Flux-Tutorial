f25fe43236537cfcfced845e0a01df54d816fdb1
sahiljain112 <sahiljain112@gmail.com>
2017-01-17 16:27:03 +0530

    Dispatcher, actions added

---
+++ src/js/actions/appActions.js
@@ -0,0 +1,29 @@
+import AppConstants from './constants/appConstants'
+import { dispatch, register } from './dispatchers/appDispatcher'
+
+export default {
+  addItem (item) {
+    dispatch({
+      actionType: AppConstants.ADD_ITEM,
+      item
+    })
+  },
+  removeItem (item) {
+    dispatch({
+      actionType: AppConstants.REMOVE_ITEM,
+      item
+    })
+  },
+  increaseItem (item) {
+    dispatch({
+      actionType: AppConstants.INCREASE_ITEM,
+      item
+    })
+  },
+  decreaseItem (item) {
+    dispatch({
+      actionType: AppConstants.DECREASE_ITEM,
+      item
+    })
+  }
+}
